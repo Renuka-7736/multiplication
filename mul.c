e149059ea44aa692538d6b6a2bc77d4911b8e887
Renuka-7736 <89129999+Renuka-7736@users.noreply.github.com>
2021-08-18 14:14:16 +0530

    Create mul.c
    multiplication of two numbers is printing

---
+++ mul.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+int main()
+{
+  int a,b,c;
+  printf("Enter the two numbers for multiplication");
+  scanf("%d %d".&a,&b);
+  c=a*b;
+  printf("a*b=c",%d %d %d);
+  return 0;
+}
+  
+  
